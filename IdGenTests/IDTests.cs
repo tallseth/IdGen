bd500616efa40ff3c7d19c2cc8503ff3da7f2a91
Seth McCarthy <seth.mccarthy@olo.com>
2021-03-27 19:32:03 -0700

    Id Parsing: test the parse method directly.

--- IdGenTests/IDTests.cs
+++ IdGenTests/IDTests.cs
@@ -1,4 +1,6 @@
-﻿using IdGen;
+﻿using System;
+using IdGen;
+using IdGenTests.Mocks;
 using NUnit.Framework;
 
 namespace IdGenTests
@@ -9,37 +11,60 @@ namespace IdGenTests
         [Test]
         public void ID_DoesNotEqual_RandomObject()
         {
-            var g = new IdGenerator(0);
-            var i = g.FromId(0);
+            var i = FromId(0);
             Assert.IsFalse(i.Equals(new object()));
-            Assert.IsTrue(i.Equals((object)g.FromId(0)));
-            Assert.IsTrue(i != g.FromId(1));
-            Assert.IsTrue(i == g.FromId(0));
-            Assert.AreEqual(i.GetHashCode(), g.FromId(0).GetHashCode());
+            Assert.IsTrue(i.Equals((object)FromId(0)));
+            Assert.IsTrue(i != FromId(1));
+            Assert.IsTrue(i == FromId(0));
+            Assert.AreEqual(i.GetHashCode(), FromId(0).GetHashCode());
         }
 
         [Test]
         public void ID_Equals_OtherId()
         {
-            var g = new IdGenerator(0);
-            var i = g.FromId(1234567890);
-            Assert.IsTrue(i.Equals(g.FromId(1234567890)));
-            Assert.IsTrue(i.Equals((object)g.FromId(1234567890)));
-            Assert.IsTrue(i != g.FromId(0));
-            Assert.IsTrue(i == g.FromId(1234567890));
-            Assert.AreEqual(i.GetHashCode(), g.FromId(1234567890).GetHashCode());
+            var i = FromId(1234567890);
+            Assert.IsTrue(i.Equals(FromId(1234567890)));
+            Assert.IsTrue(i.Equals((object)FromId(1234567890)));
+            Assert.IsTrue(i != FromId(0));
+            Assert.IsTrue(i == FromId(1234567890));
+            Assert.AreEqual(i.GetHashCode(), FromId(1234567890).GetHashCode());
         }
 
         [Test]
         public void X()
         {
-            var g = new IdGenerator(0);
-            var i = g.FromId(1);
+            var options = new IdGeneratorOptions();
+            var i = Id.Parse(1, options.IdStructure, options.TimeSource);
 
             Assert.AreEqual(1, i.SequenceNumber);
             Assert.AreEqual(0, i.GeneratorId);
-            Assert.AreEqual(g.Options.TimeSource.Epoch, i.DateTimeOffset);
+            Assert.AreEqual(options.TimeSource.Epoch, i.DateTimeOffset);
+        }
+        
+        [Test]
+        public void Parse_Returns_CorrectValue()
+        {
+            var epoch = new DateTimeOffset(2018, 7, 31, 14, 48, 2, TimeSpan.FromHours(2));  // Just some "random" epoch...
+            var currentTicks = 5;
+            var tickDurationSeconds = 7;
+            var generatorId = 234;
+            var expectedTimeAfterEpoch = TimeSpan.FromSeconds(currentTicks * tickDurationSeconds);
+            
+            var ts = new MockTimeSource(currentTicks, TimeSpan.FromSeconds(tickDurationSeconds), epoch);
+            var s = new IdStructure(42, 8, 13); 
+
+            var number = 34 + (generatorId << s.SequenceBits) + (currentTicks << (s.SequenceBits + s.GeneratorIdBits));
+            var target = Id.Parse(number, s, ts);
+
+            Assert.AreEqual(34, target.SequenceNumber);
+            Assert.AreEqual(generatorId, target.GeneratorId);
+            Assert.AreEqual(epoch.Add(expectedTimeAfterEpoch), target.DateTimeOffset);
         }
 
+        private Id FromId(long value)
+        {
+            var options = new IdGeneratorOptions();
+            return Id.Parse(value, options.IdStructure, options.TimeSource);
+        }
     }
 }
