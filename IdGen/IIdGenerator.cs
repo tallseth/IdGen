863a0fc411c1566523f37115a3a4b4350a6ed103
Seth McCarthy <seth.mccarthy@olo.com>
2021-03-29 05:06:35 -0700

    Move comments to the interface

--- IdGen/IIdGenerator.cs
+++ IdGen/IIdGenerator.cs
@@ -20,14 +20,19 @@ namespace IdGen
         /// <summary>
         /// Creates a new Id.
         /// </summary>
-        /// <returns>Returns an Id.</returns>
+        /// <returns>Returns an Id based on the <see cref="IIdGenerator"/>'s epoch, generatorid and sequence.</returns>
+        /// <exception cref="InvalidSystemClockException">Thrown when clock going backwards is detected.</exception>
+        /// <exception cref="TimestampOverflowException">Thrown when timestamp overflows allotted bits.</exception>
+        /// <exception cref="SequenceOverflowException">Thrown when sequence overflows.</exception>
+        /// <remarks>Note that this method MAY throw an one of the documented exceptions.</remarks>
         long CreateId();
 
         /// <summary>
-        /// Creates a group of Ids at once.
+        /// Creates a group of Ids, given a number of desired ID's
         /// </summary>
-        /// <param name="number">desired number of Ids</param>
-        /// <returns>An <see cref="IEnumerable{T}"/> of Ids</returns>
+        /// <param name="number">The number of ID's desired</param>
+        /// <returns>An <see cref="IEnumerable{T}"/> of ID's of length <param name="number"/></returns>
+        /// <remarks>Note that this method MAY throw any one of the exceptions documented for <see cref="CreateId"/></remarks>
         IEnumerable<long> CreateManyIds(int number);
     }
 }
--- IdGen/IdGenerator.cs
+++ IdGen/IdGenerator.cs
@@ -46,24 +46,10 @@ namespace IdGen
             return new IdGenerator(generatorId, options);
         }
 
-        /// <summary>
-        /// Gets the Id of the generator.
-        /// </summary>
         public int Id => _generatorid;
 
-        /// <summary>
-        /// Gets the <see cref="IdGeneratorOptions"/>.
-        /// </summary>
         public IdGeneratorOptions Options { get; }
-
-        /// <summary>
-        /// Creates a new Id.
-        /// </summary>
-        /// <returns>Returns an Id based on the <see cref="IdGenerator"/>'s epoch, generatorid and sequence.</returns>
-        /// <exception cref="InvalidSystemClockException">Thrown when clock going backwards is detected.</exception>
-        /// <exception cref="TimestampOverflowException">Thrown when timestamp overflows allotted bits.</exception>
-        /// <exception cref="SequenceOverflowException">Thrown when sequence overflows.</exception>
-        /// <remarks>Note that this method MAY throw an one of the documented exceptions.</remarks>
+        
         public long CreateId()
         {
             lock (_genlock)
@@ -88,12 +74,7 @@ namespace IdGen
                 return Options.IdStructure.Encode(timeslot, _generatorid, Options.SequenceGenerator.GetNextValue());
             }
         }
-
-        /// <summary>
-        /// Creates a group of Ids, given a number of desired ID's
-        /// </summary>
-        /// <param name="number">The number of ID's desired</param>
-        /// <returns>An <see cref="IEnumerable{T}"/> of ID's of length <param name="number"/></returns>
+        
         public IEnumerable<long> CreateManyIds(int number)
         {
             return Enumerable.Range(0, number).Select(_ => CreateId());
